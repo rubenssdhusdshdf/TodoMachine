1ead17945491012849a42703b24168e66afded00
Ruben <rubenvegajusto@gmail.com>
2023-11-28 16:18:16 +0000

    adding checked and deleted icons

---
+++ src/DeleteIcon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { TodoIcon } from './TodoIcon';
+
+
+function DeleteIcon() {
+    return (
+        <TodoIcon
+            type = "delete"
+            color = "red"
+        />
+    )
+}
+
+
+export { DeleteIcon };
